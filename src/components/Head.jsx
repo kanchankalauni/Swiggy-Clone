2cc8755737d97f08c79b0ba206b9689d74523aca
Kanchan Kalauni <kanchankalauni03@gmail.com>
2025-01-09 22:44:35 +0530

    updated search

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -41,7 +41,7 @@ function Head() {
 
     async function searchResultFun(val) {
         console.log(val)
-        const res = await fetch("https://www.swiggy.com/dapi/misc/place-autocomplete?input=mumbai");
+        const res = await fetch(`https://www.swiggy.com/dapi/misc/place-autocomplete?input=${val}`);
         const data = await res.json();
         setSearchResult(data.data)
     }
