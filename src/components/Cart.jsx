f8ff793b84da3e21e5d91a148f2e01ed4b6ab279
Kanchan Kalauni <kanchankalauni03@gmail.com>
2025-04-05 15:21:43 +0530

    added price in cart items and clear cart button

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,8 +3,19 @@ import { CartContext } from '../context/contextApi'
 import { Link } from 'react-router-dom'
 
 function Cart() {
-    const {cartData, setCartData} = useContext(CartContext)
+    const { cartData, setCartData } = useContext(CartContext)
     console.log(cartData)
+    // let totalPrice = 0
+    // for (let i = 0; i < cartData.length; i++) {
+    //     totalPrice = totalPrice + (cartData[i].price / 100 || cartData[i].defaultPrice) / 100
+    // }
+
+    // let totalPrice = cartData.reduce((acc, curVal) => (acc + (curVal.price / 100 || curVal.defaultPrice / 100)), 0)
+    let totalPrice = cartData.reduce((acc, curVal) => {
+        const price = curVal.price ?? curVal.defaultPrice;
+        console.log(price)
+        return acc + price / 100;
+    }, 0);
 
     function handleRemoveFromCart(i) {
         let newArr = [...cartData]
@@ -13,7 +24,13 @@ function Cart() {
         localStorage.setItem("cartData", JSON.stringify(newArr))
     }
 
-    if(cartData.length === 0){
+    function handleClearCart() {
+        setCartData([])
+        // localStorage.setItem("cartData", JSON.stringify([]))
+        localStorage.clear()
+    }
+
+    if (cartData.length === 0) {
         return <div className='w-full'>
             <div className='w-[50%] mx-auto'>
                 <h1>Kuch order krle bhai bhuka marega kya....</h1>
@@ -22,23 +39,30 @@ function Cart() {
         </div>
     }
 
-  return (
-    <div className='w-full'>
-        <div className='w-[50%] mx-auto'>
-            {
-                cartData.map((data, i) => (
+    return (
+        <div className='w-full'>
+            <div className='w-[50%] mx-auto'>
+                {
+                    cartData.map((data, i) => (
                         <div className='flex w-full justify-between my-5 p-2'>
-                            <h2 className='w-[70%] text-3xl'>{data.name}</h2>
+                            <div className='w-[70%] '>
+                                <h2 className='text-3xl'>{data.name}</h2>
+                                <p className='mt-2'>₹{data.price / 100 || data.defaultPrice / 100}</p>
+                            </div>
                             <div className='w-[30%] relative h-full'>
                                 <img className='rounded-xl aspect-square' src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/" + data.imageId} alt="" />
                                 <button onClick={() => handleRemoveFromCart(i)} className='bg-red-600 text-white absolute bottom-[-20px] left-5 text-lg font-bold rounded-xl border px-10 py-2 drop-shadow'>Remove</button>
                             </div>
                         </div>
-                ))
-            }
+                    ))
+                }
+
+                <h1 className='my-2'>Total Price - ₹{totalPrice}</h1>
+                <button onClick={handleClearCart} className='p-3 bg-green-600 rounded-lg my-7'>Clear Cart</button>
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Cart
