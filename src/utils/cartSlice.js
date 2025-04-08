50fff373ed844805a211856f33ab91ea0ae16e39
Kanchan Kalauni <kanchankalauni03@gmail.com>
2025-04-08 10:42:48 +0530

    add to cart updated

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -1,25 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
-    name : "cartSlice",
-    initialState : {
-        cartItems : JSON.parse(localStorage.getItem("cartData")) || [],
-        resInfo : JSON.parse(localStorage.getItem("resInfo")) || []
+    name: "cartSlice",
+    initialState: {
+        cartItems: JSON.parse(localStorage.getItem("cartData")) || [],
+        resInfo: JSON.parse(localStorage.getItem("resInfo")) || []
     },
-    reducers : {
-        addToCart : (state, action) => {
+    reducers: {
+        addToCart: (state, action) => {
             // console.log(action)
-            const {info, resInfo} = action.payload
+            const { info, resInfo } = action.payload
             // setCartData((prev) => [...prev, info])
-            state.cartItems = [...state.cartItems , info]
+            state.cartItems = [...state.cartItems, info]
+            state.resInfo = resInfo
             localStorage.setItem("cartData", JSON.stringify(state.cartItems))
             localStorage.setItem("resInfo", JSON.stringify(resInfo))
         },
-        deleteItem : (state, action) => {
+        deleteItem: (state, action) => {
             state.cartItems = action.payload
             localStorage.setItem("cartData", JSON.stringify(action.payload))
         },
-        clearCart : (state) => {
+        clearCart: (state) => {
             state.cartItems = []
             localStorage.removeItem("cartData")
             localStorage.removeItem("resInfo")
@@ -27,6 +28,6 @@ const cartSlice = createSlice({
     }
 })
 
-export const {addToCart, deleteItem, clearCart} = cartSlice.actions;
+export const { addToCart, deleteItem, clearCart } = cartSlice.actions;
 
 export default cartSlice.reducer
